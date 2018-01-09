33652ea9c94d614dcccacaff5fd071284d536ff8
panjiangsu <18905176668@189.cn>
2018-01-09 14:41:45 +0800

    2018/1/9

---
+++ scripts/sample.js
@@ -0,0 +1,9 @@
+var myHeading=document.querySelector("h1");
+myHeading.onclick=function(){
+    myHeading.innerText="A GOOD POEM";  
+}
+
+document.querySelector("html").onclick=function(){
+    window.alert("Don't click me again.")
+}
+
